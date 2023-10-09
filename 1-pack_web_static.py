87f03d83a7b41cc5102202d6edefade18d040dfe
Jesufemi-A <jesufemia1@gmail.com>
2023-10-09 23:40:44 +0000

    added files for AirBnbV2 clone

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""function to  generate aa .tgz archive
+"""
+from fabric.api import local
+from datetime import datetime
+from fabric.decorators import runs_once
+
+
+@runs_once
+def do_pack():
+    """ function to generate a a .tgz archive file steming from
+    project web_static folder of your AirBnB Clone repo
+    """
+
+    local("mkdir -p versions")
+    dformat = "%Y%m%d%H%M%S"
+    archive_path = "versions/web_static_{}.tgz".format(
+            datetime.strftime(datetime.now(), dformat))
+    result = local("tar -cvzf {} web_static".format(archive_path))
+    if result.failed:
+        return None
+    return archive_path
